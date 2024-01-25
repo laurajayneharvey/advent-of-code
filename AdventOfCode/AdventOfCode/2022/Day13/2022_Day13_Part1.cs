186df2662ecc3d60ee71f42ec0a0cd6ddc4ad2cb
laura.harvey <laura.harvey@neighbourly.com>
2024-01-25 12:45:22 +0000

    Starting to put existing solutions into single solution, with unit tests for sample and real inputs

---
+++ AdventOfCode/AdventOfCode/2022/Day13/2022_Day13_Part1.cs
@@ -0,0 +1,46 @@
+﻿namespace AdventOfCode._2022.Day13
+{
+    public class _2022_Day13_Part1
+    {
+        private readonly List<(string left, string right)> _pairs = [];
+        private readonly Day13 _day13;
+        private readonly Dictionary<(int pairIndex, string id), string> _leftArrays = [];
+        private readonly Dictionary<(int pairIndex, string id), string> _rightArrays = [];
+
+        public _2022_Day13_Part1()
+        {
+            _day13 = new Day13();
+        }
+
+        public int Run(string input)
+        {
+            var packetPairs = input.Split("\r\n\r\n");
+            for (var i = 0; i < packetPairs.Length; i++)
+            {
+                var packetsInPair = packetPairs[i].Split("\r\n");
+
+                var left = _day13.CreateArrays(i, packetsInPair[0], _leftArrays);
+                var right = _day13.CreateArrays(i, packetsInPair[1], _rightArrays);
+
+                _pairs.Add((left, right));
+            }
+
+            return GetResult();
+        }
+
+        private int GetResult()
+        {
+            var good = new List<int>();
+            for (var i = 0; i < _pairs.Count; i++)
+            {
+                var result = _day13.ProcessArray(i, i, _pairs[i].left, _pairs[i].right, _leftArrays, _rightArrays);
+                if (result != null && result == true)
+                {
+                    good.Add(i + 1);
+                }
+            }
+
+            return good.Aggregate(0, (a, b) => a + b);
+        }
+    }
+}
