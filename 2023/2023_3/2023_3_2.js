518cde3e4aa2d97dc8d18ee220b135fc2bfc96d7
Laura Harvey <12628991+laurajayneharvey@users.noreply.github.com>
2024-12-01 17:44:55 +0000

    Plural of elf is elves + not writing Typescript here + VS Code auto-formatting changed files

--- 2023/2023_3/2023_3_2.js
+++ 2023/2023_3/2023_3_2.js
@@ -2,62 +2,62 @@ var getSurrounding = (rows, cols) => {
   var surrounding = [];
   // row above
   if (i - 1 >= 0) {
-    var north = rows[i-1].split('');
+    var north = rows[i - 1].split('');
     if (j - 1 >= 0) {
-      var NW = north[j-1];
+      var NW = north[j - 1];
       if (NW === '*') {
-        surrounding.push({"rowIndex": i-1, "colIndex": j-1});
+        surrounding.push({ "rowIndex": i - 1, "colIndex": j - 1 });
       }
     }
     var N = north[j];
     if (N === '*') {
-      surrounding.push({"rowIndex": i-1, "colIndex": j});
+      surrounding.push({ "rowIndex": i - 1, "colIndex": j });
     }
     if (j + 1 < cols.length) {
-      var NE = north[j+1];
+      var NE = north[j + 1];
       if (NE === '*') {
-        surrounding.push({"rowIndex": i-1, "colIndex": j+1});
+        surrounding.push({ "rowIndex": i - 1, "colIndex": j + 1 });
       }
     }
   }
   // same row
   if (j - 1 >= 0) {
-    var W = cols[j-1];
+    var W = cols[j - 1];
     if (W === '*') {
-      surrounding.push({"rowIndex": i, "colIndex": j-1});
+      surrounding.push({ "rowIndex": i, "colIndex": j - 1 });
     }
   }
   if (j + 1 < cols.length) {
-    var E = cols[j+1];
+    var E = cols[j + 1];
     if (E === '*') {
-      surrounding.push({"rowIndex": i, "colIndex": j+1});
+      surrounding.push({ "rowIndex": i, "colIndex": j + 1 });
     }
   }
   // row below
   if (i + 1 < rows.length) {
-    var south = rows[i+1].split('');
+    var south = rows[i + 1].split('');
     if (j - 1 >= 0) {
-      var SW = south[j-1];
+      var SW = south[j - 1];
       if (SW === '*') {
-        surrounding.push({"rowIndex": i+1, "colIndex": j-1});
+        surrounding.push({ "rowIndex": i + 1, "colIndex": j - 1 });
       }
     }
     var S = south[j];
     if (S === '*') {
-      surrounding.push({"rowIndex": i+1, "colIndex": j});
+      surrounding.push({ "rowIndex": i + 1, "colIndex": j });
     }
     if (j + 1 < cols.length) {
-      var SE = south[j+1];
+      var SE = south[j + 1];
       if (SE === '*') {
-        surrounding.push({"rowIndex": i+1, "colIndex": j+1});
+        surrounding.push({ "rowIndex": i + 1, "colIndex": j + 1 });
       }
     }
   }
   return surrounding
 }
 
-var input = 
-`467..114..
+var input =
+  `467..114..
 ...*......
 ..35..633.
 ......#...
@@ -79,7 +79,7 @@ for (i = 0; i < rows.length; i++) {
       var possibleGearsHere = getSurrounding(rows, cols);
       if (possibleGearsHere.length) {
         var partNumber = col;
-      
+
         // find numbers before
         var k = j - 1;
         while (k >= 0 && !isNaN(cols[k])) {
@@ -93,7 +93,7 @@ for (i = 0; i < rows.length; i++) {
           l++;
           j = l;
         }
-        
+
         possibleGearsHere.forEach(x => x.partNumber = partNumber);
         possibleGearsOverall.push(...possibleGearsHere);
       }
@@ -101,22 +101,22 @@ for (i = 0; i < rows.length; i++) {
   }
 }
 
-const possibleGears = possibleGearsOverall.reduce((group, possibleGear) => {
-  const { rowIndex, colIndex, partNumber } = possibleGear;
-  
+var possibleGears = possibleGearsOverall.reduce((group, possibleGear) => {
+  var { rowIndex, colIndex, partNumber } = possibleGear;
+
   var existingGearRatio = group[`${rowIndex}-${colIndex}`]?.gearRatio ?? 1;
   var newGearRatio = existingGearRatio *= parseInt(partNumber);
-  
+
   var existingCount = group[`${rowIndex}-${colIndex}`]?.count ?? 0;
   existingCount++;
-  
-  group[`${rowIndex}-${colIndex}`] = {"gearRatio": newGearRatio, "count": existingCount};
+
+  group[`${rowIndex}-${colIndex}`] = { "gearRatio": newGearRatio, "count": existingCount };
   return group;
 }, {});
 
-const gearRatioSum = Object.keys(possibleGears)
+var gearRatioSum = Object.keys(possibleGears)
   .filter((key) => possibleGears[key].count === 2)
   .map((key) => possibleGears[key].gearRatio)
   .reduce((sum, current) => sum + current, 0);
-  
+
 console.log(gearRatioSum);
