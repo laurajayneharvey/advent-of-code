87a67b32f3256ff40ddb0aac7f040a55fcb55029
NBRLY-DEV-LH\Laura Harvey <laura.harvey@neighbourly.com>
2025-12-02 05:33:27 +0000

    2025 Day 2 Part 1

--- AdventOfCode/AdventOfCode/2025/Day2/Day2_Part1.cs
+++ AdventOfCode/AdventOfCode/2025/Day2/Day2_Part1.cs
@@ -2,9 +2,36 @@
 {
     public class Day2_Part1
     {
-        public int Run(string input)
+        public ulong Run(string input)
         {
-            return 0;
+            var ranges = input.Split(',').Select(x =>
+            {
+                var endpoints = x.Split('-');
+                return endpoints.Select(ulong.Parse).ToList();
+            });
+
+            ulong invalidIdSum = 0;
+            foreach (var range in ranges)
+            {
+                for (var i = range[0]; i <= range[1]; i++)
+                {
+                    var asString = i.ToString();
+                    var length = asString.Length;
+                    if (length % 2 != 0)
+                    {
+                        continue;
+                    }
+
+                    var a = asString[..(length / 2)];
+                    var b = asString.Substring(length / 2, length / 2);
+                    if (a == b)
+                    {
+                        invalidIdSum += i;
+                    }
+                }
+            }
+
+            return invalidIdSum;
         }
     }
 }
