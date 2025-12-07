7045759546192df025b5cfbfa8d424740ac365a0
Laura Harvey <12628991+laurajayneharvey@users.noreply.github.com>
2025-12-07 16:30:10 +0000

    2025 Day 4 Part 1

--- AdventOfCode/AdventOfCode/2025/Day4/Day4_Part1.cs
+++ AdventOfCode/AdventOfCode/2025/Day4/Day4_Part1.cs
@@ -4,7 +4,78 @@
     {
         public int Run(string input)
         {
-            return 0;
+            /*
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+             */
+
+            var rows = input.Split("\r\n");
+            var coordinates = new List<Coordinate>();
+            for (var rowIndex = 0; rowIndex < rows.Length; rowIndex++)
+            {
+                var columns = rows[rowIndex].ToCharArray();
+                for (var columnIndex = 0; columnIndex < rows[rowIndex].Length; columnIndex++)
+                {
+                    coordinates.Add(new Coordinate
+                    {
+                        X = columnIndex,
+                        Y = rowIndex,
+                        Value = columns[columnIndex]
+                    });
+                }
+            }
+
+            var wallpaperCoordinates = coordinates.Where(c => c.IsWallpaper);
+            foreach (var wallpaperCoordinate in wallpaperCoordinates)
+            {
+                var wallpaperAdjacentCount = 0;
+                foreach(var (X, Y) in wallpaperCoordinate.Adjacents)
+                {
+                    var coordinate = coordinates.FirstOrDefault(c => c.X == X && c.Y == Y);
+                    if (coordinate != null && coordinate.IsWallpaper)
+                    {
+                        wallpaperAdjacentCount++;
+                    }
+                    if (wallpaperAdjacentCount == 4)
+                    {
+                        break;
+                    }
+                }
+                if (wallpaperAdjacentCount < 4)
+                {
+                    wallpaperCoordinate.Accessible = true;
+                }
+            }
+
+            return wallpaperCoordinates.Count(c => c.Accessible);
+        }
+
+        private class Coordinate
+        {
+            public int X;
+            public int Y;
+
+            public bool IsWallpaper => Value == '@';
+            public char Value;
+            public bool Accessible = false;
+
+            public (int X, int Y) North => new(X, Y - 1);
+            public (int X, int Y) South => new(X, Y + 1);
+            public (int X, int Y) East => new(X + 1, Y);
+            public (int X, int Y) West => new(X - 1, Y);
+            public (int X, int Y) NorthWest => new(West.X, North.Y);
+            public (int X, int Y) NorthEast => new(East.X, North.Y);
+            public (int X, int Y) SouthWest => new(West.X, South.Y);
+            public (int X, int Y) SouthEast => new(East.X, South.Y);
+            public IList<(int X, int Y)> Adjacents => [North, South, East, West, NorthWest, NorthEast, SouthWest, SouthEast];
         }
     }
 }
